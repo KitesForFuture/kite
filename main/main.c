3c07a213ac0ca371c3d2abd2f20a43b5d818ecb9
Your Name <you@example.com>
2021-02-25 10:05:38 +0100

    HOVER-EIGHT-LANDING-MANUAL logic

--- main/main.c
+++ main/main.c
@@ -18,6 +18,14 @@
 #include "control/figure-eight.c"
 #include "control/hover.c"
 
+#define FIGURE_EIGHT 0
+#define HOVER 1
+#define LANDING 2
+#define MANUAL 3
+
+#define LEFT -1
+#define RIGHT 1
+
 struct i2c_bus bus0 = {14, 25};
 struct i2c_bus bus1 = {18, 19};
 
@@ -43,6 +51,10 @@ void app_main(void)
 	int input_pins[] = {4, 33, 2, 17, 16};
 	initPWMInput(input_pins, 5);
     
+    
+    int FLIGHT_MODE = FIGURE_EIGHT;
+    int DIRECTION = LEFT;
+    
     while(1) {
         vTaskDelay(1);
 
@@ -52,32 +64,54 @@ void app_main(void)
         
         updatePWMInput();
 		
-        //printf("BMP280 Height: %f\n", getHeight());
-		
-		
         // FIGURE EIGHT PREPARATION: HOLD NOSE STRAIGHT UP
-        getPWMInputMinus1to1normalized(0);
-        getPWMInputMinus1to1normalized(1);
-        float target_angle = 1.2*3.1415926535*0.5*getPWMInputMinus1to1normalized(2);
+        float CH1 = getPWMInputMinus1to1normalized(0);
+        float CH2 = getPWMInputMinus1to1normalized(1);
+        float CH3 = getPWMInput0to1normalized(2);
+        // CH4 not used
+        float CH5 = getPWMInputMinus1to1normalized(3);
+        float CH6 = getPWMInputMinus1to1normalized(4);
         
-        // MANUAL MODE	
-        //rudder_angle = -60*getPWMInputMinus1to1normalized(2)+getRudderControl(0, 0, (float)(pow(10,getPWMInputMinus1to1normalized(0))));
+        float rudder_angle = 0;
+        float elevator_angle = 0;
+        float propeller_speed = 0;
         
-        float rudder_angle = getHoverRudderControl(0, 1, 1);
+        if (FLIGHT_MODE == HOVER) {
         
+        	float goal_height = 1.5;
+        	float rate_of_climb = 0.3;
+        	rudder_angle = getHoverRudderControl(0, 1, 1);
+		    
+		    if(rudder_angle > 45) rudder_angle = 45;
+		    if(rudder_angle < -45) rudder_angle = -45;
+		    
+		    elevator_angle = -getHoverElevatorControl(0, 1, 1);
+		    
+		    propeller_speed = 30 + getHoverHeightControl(goal_height, rate_of_climb, 1, 1);
+		    
+        } else if (FLIGHT_MODE == FIGURE_EIGHT) {
         
-        if(rudder_angle > 45) rudder_angle = 45;
-        if(rudder_angle < -45) rudder_angle = -45;
+        	float target_angle = 1.2*3.1415926535*0.5*DIRECTION*1;// 1 means 1.2*90 degrees, 0 means 0 degrees
+        	rudder_angle = getRudderControl(target_angle, 1, 1);
+        	
+        } else if (FLIGHT_MODE == LANDING) {
         
+			rudder_angle = 0;
+			elevator_angle = 20; // TODO: find right angle for stall landing
+        	
+        } else if (FLIGHT_MODE == MANUAL) {
         
-        float elevator_angle = getHoverElevatorControl(0, 1, 1);
+        	rudder_angle = 60*CH1;
+        	elevator_angle = 60*CH2;
+        	propeller_speed = 90*CH3;
+        	
+        }
         
         setAngle(0, rudder_angle);
-        setAngle(1, -elevator_angle);
+		setAngle(1, elevator_angle);
+		setSpeed(2, propeller_speed);
+		setSpeed(3, propeller_speed);
         
-        float propeller_speed = 30 + getHoverHeightControl(1.5, 0.3, 1, 1);
-        setSpeed(2, propeller_speed);
-        setSpeed(3, propeller_speed);
         
         //printf("%f, %f\n", getHeightDerivative(), getHeight());
         // SENDING DEBUGGING DATA TO GROUND
