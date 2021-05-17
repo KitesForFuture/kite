3a194ab75e4c48c368e597e3ee6390c1b33dcb38
leonardkoll <leonard.koll@netlight.com>
2021-05-17 15:39:29 +0200

    byte based logging

--- main/control/rotation_matrix.h
+++ main/control/rotation_matrix.h
@@ -10,15 +10,14 @@ class RotationMatrix {
     float x_gravity_factor, y_gravity_factor, z_gravity_factor;
     MsTimer timer {};
     // rotation of the drone in world coordinates
-    float matrix[9] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+    float* matrix;
 
     void rotate_towards_g(float mat[], float a, float b, float c);
 
 public:
 
-    explicit RotationMatrix(float gravity[]);
+    explicit RotationMatrix(float* matrix, float gravity[]);
     void update(struct motion_data position);
-    void print(); // ToDo Leo remove: Logging instead.
 };
 
 #endif
